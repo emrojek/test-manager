de35575a23adf7d1fb0db6c1366e1f29ddbd0794
emrojek <michal.rojek.it@gmail.com>
2025-09-26 14:48:46 +0200

    docs: Add JSDoc to createTestFile function

--- src/services/file-generator.ts
+++ src/services/file-generator.ts
@@ -1,6 +1,15 @@
 import fs from 'fs/promises';
 import path from 'path';
 
+/**
+ * Creates a new test file based on a template.
+ * @param targetDir - The directory where the new test file will be created.
+ * @param targetName - The name of the new test file (without extension, e.g., 'login form').
+ * @param testSuiteName - The name for the test.describe() block.
+ * @param testCaseName - The name for the test() block.
+ * @returns A promise that resolves to the path of the newly created test file.
+ */
+
 export const createTestFile = async (
     targetDir: string,
     targetName: string,
